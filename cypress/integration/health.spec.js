78e933cbb4cbb72eb5455fbaf6706baae250e8de
Tamara Santos <tamara.santos@MacBook-Pro.local>
2021-01-29 16:52:54 -0300

    Fix lint

--- cypress/integration/health.spec.js
+++ cypress/integration/health.spec.js
@@ -1,13 +1,11 @@
-const { expect } = require("chai")
+const { expect } = require('chai')
 
 describe('Endpoints de Healthcheck', () => {
   it('Verificar primeira rota de saúde', () => {
     cy.fixture('health.json').then((data) => {
-      cy.makeRequest(
-        `${data.urlBase}/health`,
-        'GET',
-        data.headers
-      ).as('healthcheck')
+      cy.makeRequest(`${data.urlBase}/health`, 'GET', data.headers).as(
+        'healthcheck',
+      )
 
       cy.get('@healthcheck').should((res) => {
         expect(res.status).to.be.equal(200)
@@ -17,11 +15,9 @@ describe('Endpoints de Healthcheck', () => {
 
   it('Verificar segunda rota de saúde', () => {
     cy.fixture('health.json').then((data) => {
-      cy.makeRequest(
-        `${data.urlBase}/health2`,
-        'GET',
-        data.headers
-      ).as('healthcheck2')
+      cy.makeRequest(`${data.urlBase}/health2`, 'GET', data.headers).as(
+        'healthcheck2',
+      )
 
       cy.get('@healthcheck2').should((res) => {
         expect(res.status).to.be.equal(200)
--- cypress/integration/paymentFlows.spec.js
+++ cypress/integration/paymentFlows.spec.js
@@ -1,47 +1,47 @@
-const { expect } = require("chai")
+const { expect } = require('chai')
 
 describe('Fluxos de Pagamento', () => {
-    it('Receber arquivo JSON com solicitação de pagamento', () => {
-        cy.fixture('payments.json').then((data) => {
-            cy.makeRequest(
-                `${data.urlBase}/ecommerce/public/payments`,
-                'POST',
-                data.headersPaymentJson,
-                data.bodyPaymentJson
-            ).as('paymentJson')
-        })
-
-        cy.get('@paymentJson').should((res) => {
-            expect(res.status).to.be.equal(200) //401
-        })
+  it('Receber arquivo JSON com solicitação de pagamento', () => {
+    cy.fixture('payments.json').then((data) => {
+      cy.makeRequest(
+        `${data.urlBase}/ecommerce/public/payments`,
+        'POST',
+        data.headersPaymentJson,
+        data.bodyPaymentJson,
+      ).as('paymentJson')
     })
 
-    it('Consulta status de uma transação de pagamento', () => {
-        cy.fixture('payments.json').then((data) => {
-            cy.makeRequest(
-                `${data.urlBase}/ecommerce/public/payments/${data.referenceId}/status`,
-                'GET',
-                data.headersPayment
-            ).as('statusPayment')
-        })
+    cy.get('@paymentJson').should((res) => {
+      expect(res.status).to.be.equal(200) //401
+    })
+  })
 
-        cy.get('@statusPayment').should((res) => {
-            expect(res.status).to.be.equal(200) //401
-        })
+  it('Consulta status de uma transação de pagamento', () => {
+    cy.fixture('payments.json').then((data) => {
+      cy.makeRequest(
+        `${data.urlBase}/ecommerce/public/payments/${data.referenceId}/status`,
+        'GET',
+        data.headersPayment,
+      ).as('statusPayment')
     })
 
-    it('Receber um pagamento via PicPay App', () =>{
-        cy.fixture('payments.json').then((data) => {
-            cy.makeRequest(
-                `${data.urlBase}/ecommerce/app/payments/${data.orderId}`,
-                'POST',
-                data.headersPaymentApp,
-                data.bodyPaymentApp
-            ).as('paymentApp')
-        })
-
-        cy.get('@paymentApp').should((res) => {
-            expect(res.status).to.be.equal(200) //422
-        })
+    cy.get('@statusPayment').should((res) => {
+      expect(res.status).to.be.equal(200) //401
     })
+  })
+
+  it('Receber um pagamento via PicPay App', () => {
+    cy.fixture('payments.json').then((data) => {
+      cy.makeRequest(
+        `${data.urlBase}/ecommerce/app/payments/${data.orderId}`,
+        'POST',
+        data.headersPaymentApp,
+        data.bodyPaymentApp,
+      ).as('paymentApp')
+    })
+
+    cy.get('@paymentApp').should((res) => {
+      expect(res.status).to.be.equal(200) //422
+    })
+  })
 })
