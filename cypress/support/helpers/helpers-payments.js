c09619155b83c7f1bcc8cbbd6d90a5549244ee48
Tamara Ferreira <tamara.santos@picpay.com>
2021-07-13 10:43:23 -0300

    Correção de nome da variável do helper

--- cypress/support/helpers/helpers-payments.js
+++ cypress/support/helpers/helpers-payments.js
@@ -2,8 +2,8 @@ class HelpersPayment {
   helperBodyPaymentIntent(){
     cy.readFile('cypress/fixtures/payments.json').then((data) => {
       let previousData = data
-      let teste = Number(previousData.paymentIntentBody.referenceId) + 1
-      previousData.paymentIntentBody.referenceId = teste.toString()
+      let futureData = Number(previousData.paymentIntentBody.referenceId) + 1
+      previousData.paymentIntentBody.referenceId = futureData.toString()
       cy.writeFile('cypress/fixtures/payments.json', JSON.stringify(previousData))
     })
   }
